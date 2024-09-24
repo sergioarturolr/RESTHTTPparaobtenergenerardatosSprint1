f78f4d83a34cf0d805e587411fd14320bad9e096
sergioarturolr <sergioarturolr@hotmail.com>
2024-09-24 15:21:34 -0600

    Variables de tipo numerico

--- index.ts
+++ index.ts
@@ -11,3 +11,18 @@ console.log('Variables de tipo primitivo')
 console.log(flag)
 console.log(yes)
 console.log(no)
+
+/**
+ * Variables de tipo numérico
+ */
+
+let x: number; //Declaración explicita
+x = 10
+let y = 0; //Declaración implicita
+let z: number = 123.456; //Declaración explicita
+//let big: bigint = 100n;
+
+console.log('Variables de tipo numérico')
+console.log(x)
+console.log(y)
+console.log(z)
