d75b24d2954df39a417c6978e1a4353db31e92e6
sergioarturolr <sergioarturolr@hotmail.com>
2024-09-24 14:06:55 -0600

    Variables de tipo primitivo

---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * Variables de tipo primitivo
+ */
+let flag: boolean; //Declaración explícita
+let yes = true; //Declaración implícita
+let no = false; //Declaración implícita
+
+flag = true;
+
+console.log('Variables de tipo primitivo')
+console.log(flag)
+console.log(yes)
+console.log(no)
