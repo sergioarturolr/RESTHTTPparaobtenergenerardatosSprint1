bbbd956ed4afa8eb575d1af761a1bc883a805024
sergioarturolr <sergioarturolr@hotmail.com>
2024-09-25 12:08:50 -0600

    Variables EXERCISE 1

--- index.ts
+++ index.ts
@@ -144,4 +144,24 @@ console.log(person1)
 
 //let person3: [string, number] = [35, 'Marcia'];//error: type number is not asiggnable to string and string is not assignable to number
 
+/*  
+**EXERCISE 1
+*/
 
+    let firstName: string;;
+    let lastName: string;;
+    let fullName: string;;
+    let age : number;;
+    let ukCitizen: boolean;;
+    
+    firstName = 'Rebecca';
+    lastName = 'Smith';
+    age = 42;
+    ukCitizen = false;
+    fullName = firstName + " " + lastName;
+    
+    if (ukCitizen) {
+        console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
+    } else {
+        console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
+    }
